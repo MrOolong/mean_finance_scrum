dad9e203cea6ad3bfa891f4aec1dc842a45fef56
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-09-27 00:28:37 +0000

    added document.ready function

--- public/css/colorswitcher.js
+++ public/css/colorswitcher.js
@@ -4,7 +4,8 @@
 //     document.getElementsByTagName('body')[0].style.backgroundColor = 'gray'; 
 //     document.getElementsByClassName('night')[0].style.color = 'white';
 // };
-
+$(document).ready(function(){
+    
 $('#switch-color').click(function(){
   $('#body-toggle').toggleClass('night');
   $('.body-toggle').toggleClass('night');
@@ -13,6 +14,8 @@ $('#switch-color').click(function(){
         });
             $('#switch-color').toggleClass('daymode-button');
             //$('.night-text').toggleClass('night-text');
+});
+
 });
 
 //better to toggle visibility of existing?
